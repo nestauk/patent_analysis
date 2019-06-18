3beebda782befd9b6edcb0dd165fe3f82cf0a617
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2019-06-18 07:05:00 +0100

    Setup Nesta Data Science cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='patents',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploratory analysis of patent data',
+    author='Juan Mateos Garcia',
+    license='MIT',
+)
